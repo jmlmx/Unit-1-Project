70bf1452c4f456c6a08faf9563e6b06e8d64c775
jmlmx <jmayon0@gmail.com>
2023-05-02 20:46:14 -0400

    Javascript functions and CSS changes

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 /*----- constants -----*/
-
+const guessLimit = 10
+const pointsAdded = 100
 /*----- state variables -----*/
-
+let playerGuesses = 0
+let playerScore = 0
+let imagePack = []
+let guessesLeft = guessLimit - playerGuesses
+let multiplier = guessesLeft
+let choice1
+let choice2
+let winner
+let finalScore = pointsAdded * guessesLeft
+let board
 /*----- cached elements  -----*/
+const boxEls = [...document.querySelectorAll('#board > div')]
+const playAgainBtn = document.querySelector('button')
 
 /*----- event listeners -----*/
-
+playAgainBtn.addEventListener('click', handleClick)
 /*----- functions -----*/
+Init()
+
+function Init() {
+board = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+]
+winner = false
+render()
+}
+
+function handleClick(evt) {
+    const colIdx = boxEls.indexOf(evt.target)
+}
