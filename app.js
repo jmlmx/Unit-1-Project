d00ffbc4adf64bb168648d8e70bd9525e272558d
jmlmx <jmayon0@gmail.com>
2023-05-06 12:32:02 -0400

    score system removal

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 /*----- constants -----*/
 const guessLimit = 10
-const pointsAdded = 100
 /*----- state variables -----*/
 let playerGuesses = 0
-let playerScore = 0
 let images = []
 let guessesLeft = guessLimit - playerGuesses
-let multiplier = guessesLeft
 let choice1
 let choice2
 let winner
-let finalScore = pointsAdded * guessesLeft
 let board
 /*----- cached elements  -----*/
 const boxEls = [...document.querySelectorAll('#board > div')]
 const playAgainBtn = document.querySelector("replay-button")
-const startBtn = document.getElementById("start")
+const startBtn = document.getElementById("#start")
 const modalEl = document.getElementsByClassName("modal")
+const playerGuessEl = document.getElementById('player-guesses')
 /*----- event listeners -----*/
 playAgainBtn.addEventListener('click', handleClick);
 startBtn.addEventListener('click', startGame);
@@ -34,12 +31,12 @@ render()
 }
 
 function startGame() {
-    
+    modalEl.classList.add('inactive');
 
 }
 
 function handleClick(evt) {
-    
+
 }
 
 function renderBoard() {
@@ -54,6 +51,5 @@ render() {
     renderBoard()
     renderGuessesLeft()
     checkWin()
-    getFinalScore()
 }
 
