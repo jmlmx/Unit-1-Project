e31f0b1d8fd031066230b411bab1f95cab6618b9
jmlmx <jmayon0@gmail.com>
2023-05-06 12:06:37 -0400

    minor changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const pointsAdded = 100
 /*----- state variables -----*/
 let playerGuesses = 0
 let playerScore = 0
-let imagePack = []
+let images = []
 let guessesLeft = guessLimit - playerGuesses
 let multiplier = guessesLeft
 let choice1
@@ -14,14 +14,13 @@ let finalScore = pointsAdded * guessesLeft
 let board
 /*----- cached elements  -----*/
 const boxEls = [...document.querySelectorAll('#board > div')]
-const playAgainBtn = document.querySelector('button')
-const startBtn = document.querySelector('start')
-
+const playAgainBtn = document.querySelector("replay-button")
+const startBtn = document.getElementById("start")
+const modalEl = document.getElementsByClassName("modal")
 /*----- event listeners -----*/
-playAgainBtn.addEventListener('click', handleClick)
-startBtn.addEventListener('click', startGame)
+playAgainBtn.addEventListener('click', handleClick);
+startBtn.addEventListener('click', startGame);
 /*----- functions -----*/
-Init()
 
 function Init() {
 board = [
@@ -35,12 +34,26 @@ render()
 }
 
 function startGame() {
-    $(".mask").addClass("inactive");
-    Init()
-    render()
+    
+
 }
 
 function handleClick(evt) {
-    const colIdx = boxEls.indexOf(evt.target)
+    
+}
+
+function renderBoard() {
+
+}
+
+function renderGuessesLeft() {
+
+}
+
+render() {
+    renderBoard()
+    renderGuessesLeft()
+    checkWin()
+    getFinalScore()
 }
 
